52dacd6df9568771857543b0c114291b015a9da7
Tushar Patel <tmpateluk@gmail.com>
2024-04-22 22:46:16 +0100

    my first home Work

---
+++ HomeWorkManualTesting.java
@@ -0,0 +1,48 @@
+package HomeWork1;
+
+public class HomeWorkManualTesting
+{
+    public static void main( String[] args )
+    {
+        System.out.println("What is Manual Testing?");
+        System.out.println("Types of Testing");
+        System.out.println("Testing Techniques");
+        System.out.println("Software Testing Life Cycle");
+        System.out.println("Software Development Life Cycle");
+        System.out.println("White Box Testing");
+        System.out.println("Black Box Testing");
+        System.out.println("Regression Testing");
+        System.out.println("Retesting");
+        System.out.println("Maintenance Testing");
+        System.out.println("Severity VS Priority");
+        System.out.println("Software Testing Methods");
+        System.out.println("Software Testing Models");
+        System.out.println("Waterfall Model");
+        System.out.println("V Model");
+        System.out.println("Agile Methodology");
+        System.out.println("What is Sprint?");
+        System.out.println("what is bug ?");
+        System.out.println("Bug Life Cycle");
+        System.out.println("Bug Reporting");
+        System.out.println("System Testing");
+        System.out.println("Unit Testing");
+        System.out.println("Iterative Model");
+        System.out.println("Verification & Validation Model");
+        System.out.println("Ad-hoc Testing");
+        System.out.println("Exploratory Testing");
+        System.out.println("Static VS Dynamic Testing");
+        System.out.println("Spiral Model");
+        System.out.println("Agile-Scrum Framework");
+        System.out.println("Test Plan");
+        System.out.println("Test Cases");
+        System.out.println("Test Scenario");
+        System.out.println("Principles of Software Testing");
+        System.out.println("Levels of Testing");
+        System.out.println("Functional testing");
+        System.out.println("Non-functional Testing");
+        System.out.println("Performance Testing");
+        System.out.println("GUI Testing");
+        System.out.println("Qualities of Good Tester");
+        System.out.println("Thank You");
+    }
+}
